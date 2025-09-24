ba741fc0ae93bc5c677292d1a7867089d91fcba0
Lucas Lefèvre (lul) <lul@odoo.com>
2025-09-24 15:41:26 +0200

    [REF] range: simpler objects

--- src/types/range.ts
+++ src/types/range.ts
@@ -8,10 +8,10 @@ export interface RangePart {
 export interface Range {
   readonly zone: Readonly<Zone>;
   readonly unboundedZone: Readonly<UnboundedZone>;
-  readonly parts: readonly RangePart[];
+  readonly parts?: readonly RangePart[];
   readonly invalidXc?: string;
   /** true if the user provided the range with the sheet name */
-  readonly prefixSheet: boolean;
+  readonly prefixSheet?: boolean;
   /** the name of any sheet that is invalid */
   readonly invalidSheetName?: string;
   /** the sheet on which the range is defined */
